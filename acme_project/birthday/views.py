5f26ed60ced51768423f0f74ef8689f8fa866ea7
Igor Zinchenko <sibnum@yandex.ru>
2026-01-04 20:05:39 +0300

    added birthday_list in project

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Birthday
 
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
@@ -14,3 +15,8 @@ def birthday(request):
         )
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+def birthday_list(request):
+    birthdays = Birthday.objects.all()
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
