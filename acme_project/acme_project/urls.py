8450bb21d185673271e427cc3dc8351b5527a30f
Igor Zinchenko <sibnum@yandex.ru>
2026-01-14 21:24:01 +0300

    added registration form

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,9 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.views.generic.edit import CreateView
+from django.urls import include, path, reverse_lazy
+
+from users.forms import CustomUserCreationForm
 
 urlpatterns = [
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=CustomUserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration'
+    ),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
