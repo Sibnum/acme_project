55598f756b2a5d9a40813323df5ccf6acad6b0cb
Igor Zinchenko <sibnum@yandex.ru>
2026-01-18 16:17:47 +0300

    changed queryset in BirthdayListView

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -21,6 +21,9 @@ class OnlyAuthorMixin(UserPassesTestMixin):
 
 class BirthdayListView(ListView):
     model = Birthday
+    queryset = Birthday.objects.prefetch_related(
+        'tags'
+    ).select_related('author')
     ordering = 'id'
     paginate_by = 10
 
